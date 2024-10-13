c497f837cfe518bb0324f5b667c45b707c5bb3f5
DoctorSulla <matthew.halliday@gmail.com>
2024-10-13 18:52:47 +0100

    Added bundler

---
+++ package.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+folder_name="christmaslist.xyz"
+app_name="christmas_lists"
+rm -rf $folder_name
+mkdir $folder_name
+cargo build --release
+cp -r assets $folder_name
+cp -r target/release/$app_name $folder_name
+tar -czvf $app_name.tar.gz $folder_name
+rm -rf $folder_name
+scp $app_name.tar.gz hroot:/srv/http/
